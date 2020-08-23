16e5fbb5d1bcfa0200c20ccbf94a645da22bb613
troyg <troy.gidney@gmail.com>
2020-08-23 13:01:07 -0600

    Setup the ConfigAccessor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,38 @@
-public class Main {
+/*
+ * Copyright (c) 2020. Troy Gidney
+ * All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * File Last Modified: 8/23/20, 1:00 PM
+ * File: Main.java
+ * Project: DiscordBotSpigot
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static Main instance;
+    public static ConfigAccessor config;
+
+    @Override
+    public void onEnable() {
+        Main.instance = this;
+
+        loadConfigurationFiles();
+
+    }
+
+    private void loadConfigurationFiles() {
+        Main.config = new ConfigAccessor(Main.instance, "config.yml");
+    }
+
+
+    @Override
+    public void onDisable() {
+
+    }
+
 }
 
